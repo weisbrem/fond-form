042f7e3c683acda94f7ac637257200c2b02e31c7
Konstantin <zhernovkov_konst@mail.ru>
2023-05-25 08:13:08 +0200

    feat: add swiper slider

---
+++ app.js
@@ -0,0 +1,21 @@
+const totalSum = document.querySelector('.slider__total-sum');
+const sum = totalSum.textContent.replace(/[^\d]/g, '');
+
+/** Миллисекунд в сутках */
+const MILLISEC_IN_DAY = 86_400_000;
+
+setInterval(() => {
+  Procent = Math.floor((Number(sum) / 100) * 0.1);
+
+  result = +sum + Procent;
+
+  totalSum.textContent = `${result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')} ₽`;
+}, MILLISEC_IN_DAY);
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.next',
+    prevEl: '.prev',
+  },
+});
